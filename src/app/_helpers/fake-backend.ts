6a4ee507a1ddbef057423b094ca12e64158a18de
Marco Taborda <marco.taborda@dbserver.com.br>
2020-03-10 19:31:00 -0300

    Fake login and home redirect

---
+++ src/app/_helpers/fake-backend.ts
@@ -0,0 +1,87 @@
+﻿import { Injectable } from '@angular/core';
+import { HttpRequest, HttpResponse, HttpHandler, HttpEvent, HttpInterceptor, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { Observable, of, throwError } from 'rxjs';
+import { delay, mergeMap, materialize, dematerialize } from 'rxjs/operators';
+
+import { User } from '@app/_models';
+
+const users: User[] = [
+    {
+        id: 1,
+        username: 'marco.taborda',
+        password: 'teste',
+        firstName: 'Marco',
+        lastName: 'Taborda'
+    },
+    {
+        id: 2,
+        username: 'ricardo.kovalski',
+        password: 'teste',
+        firstName: 'Ricardo',
+        lastName: 'Kovalski'
+    }
+];
+
+@Injectable()
+export class FakeBackendInterceptor implements HttpInterceptor {
+    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        const { url, method, headers, body } = request;
+
+        return of(null)
+            .pipe(mergeMap(handleRoute))
+            .pipe(materialize())
+            .pipe(delay(500))
+            .pipe(dematerialize());
+
+        function handleRoute() {
+            switch (true) {
+                case url.endsWith('/users/authenticate') && method === 'POST':
+                    return authenticate();
+                case url.endsWith('/users') && method === 'GET':
+                    return getUsers();
+                default:
+                    return next.handle(request);
+            }
+        }
+
+        function authenticate() {
+            const { username, password } = body;
+            const user = users.find(x => x.username === username && x.password === password);
+            if (!user) return error('Usuário e/ou senha inválidos.');
+            return ok({
+                id: user.id,
+                username: user.username,
+                firstName: user.firstName,
+                lastName: user.lastName,
+                token: 'fake-jwt-token'
+            })
+        }
+
+        function getUsers() {
+            if (!isLoggedIn()) return unauthorized();
+            return ok(users);
+        }
+
+        function ok(body?) {
+            return of(new HttpResponse({ status: 200, body }))
+        }
+
+        function error(message) {
+            return throwError({ error: { message } });
+        }
+
+        function unauthorized() {
+            return throwError({ status: 401, error: { message: 'Unauthorized' } });
+        }
+
+        function isLoggedIn() {
+            return headers.get('Authorization') === 'Bearer fake-jwt-token';
+        }
+    }
+}
+
+export let fakeBackendProvider = {
+    provide: HTTP_INTERCEPTORS,
+    useClass: FakeBackendInterceptor,
+    multi: true
+};
