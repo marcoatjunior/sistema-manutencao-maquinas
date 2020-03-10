a234d883838ad4abd7dce309c1039fe85e895e62
Marco Taborda <marco.taborda@dbserver.com.br>
2020-03-10 19:47:15 -0300

    Form rename and identing login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -5,7 +5,7 @@ import { first } from 'rxjs/operators';
 
 import { AuthenticationService } from '@app/_services';
 
-@Component({ 
+@Component({
     templateUrl: 'login.component.html',
     styleUrls: ['login.component.scss']
 })
@@ -21,9 +21,8 @@ export class LoginComponent implements OnInit {
         private route: ActivatedRoute,
         private router: Router,
         private authenticationService: AuthenticationService
-    ) { 
-        // redirect to home if already logged in
-        if (this.authenticationService.currentUserValue) { 
+    ) {
+        if (this.authenticationService.currentUserValue) {
             this.router.navigate(['/']);
         }
     }
@@ -33,32 +32,28 @@ export class LoginComponent implements OnInit {
             username: ['', Validators.required],
             password: ['', Validators.required]
         });
-
-        // get return url from route parameters or default to '/'
         this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
     }
 
-    // convenience getter for easy access to form fields
-    get f() { return this.loginForm.controls; }
+    get form() {
+        return this.loginForm.controls;
+    }
 
     onSubmit() {
         this.submitted = true;
 
-        // stop here if form is invalid
         if (this.loginForm.invalid) {
             return;
         }
 
         this.loading = true;
-        this.authenticationService.login(this.f.username.value, this.f.password.value)
+        this.authenticationService.login(this.form.username.value, this.form.password.value)
             .pipe(first())
-            .subscribe(
-                data => {
-                    this.router.navigate([this.returnUrl]);
-                },
-                error => {
-                    this.error = error;
-                    this.loading = false;
-                });
+            .subscribe(() => {
+                this.router.navigate([this.returnUrl]);
+            }, error => {
+                this.error = error;
+                this.loading = false;
+            });
     }
 }
