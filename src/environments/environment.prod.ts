24b2acd60c4d408a5bf9a484de6f84f03a9f771f
Marco Taborda <marco.taborda@dbserver.com.br>
2020-03-30 20:08:48 -0300

    Change apiUrl from production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://localhost:4000'
+    apiUrl: 'https://cors-anywhere.herokuapp.com/https://novus-heroku.herokuapp.com/api'
 };
